753aec64a52379c20b7898c32fbcfc736aca5228
Ajschlander <Aschlander21@gmail.com>
2020-02-11 13:32:37 -0600

    Added connection to db

---
+++ config/connection.js
@@ -0,0 +1,27 @@
+
+// Dependencies
+const Sequelize = require("sequelize");
+
+// Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
+const sequelize = new Sequelize("employee_tracker_db", "root", "root", {
+  host: "localhost",
+  port: 3306,
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  },
+  logging: false
+});
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+// Exports the connection for other files to use
+module.exports = sequelize;
